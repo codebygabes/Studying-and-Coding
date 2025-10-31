bf259abef80d203b9b1b1e6c8791424404118b54
gbs 。 、 <157754505+codebygabes@users.noreply.github.com>
2025-10-31 10:09:07 -0300

    Add files via upload

---
+++ POO - JavaScript/ProjetoFabricacaoDeObjetos/src/br/com/gabes/interfaces/Animal.java
@@ -0,0 +1,6 @@
+package br.com.gabes.interfaces;
+
+public interface Animal {
+
+	public void emitirSom();
+}
---
+++ POO - JavaScript/ProjetoFabricacaoDeVeiculos/src/br/com/gabes/veiculos/Bicicleta.java
@@ -0,0 +1,13 @@
+package br.com.gabes.veiculos;
+
+import br.com.gabes.interfaces.Veiculos;
+
+public class Bicicleta implements Veiculos {
+
+	@Override
+	public void emMovimento() {
+		// TODO Auto-generated method stub
+		System.out.println("Bicicleta está em movimento.");
+	}
+
+}
